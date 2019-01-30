7b7c97e4a5c43e534effb5de1d34d4759bfcfc05
Arjit <arjit32754@gmail.com>
2019-01-30 14:05:12 -0600

    finding power of x to n

---
+++ Binary Search/two halves/powXofN.java
@@ -0,0 +1,14 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1;
+        long absN = Math.abs((long)n);
+        while(absN > 0) {
+            if((absN&1)==1) {
+                ans = ans*x;
+            }
+            absN >>= 1;
+            x = x*x;
+        }
+        return n < 0 ?  1/ans : ans;
+}
+}
