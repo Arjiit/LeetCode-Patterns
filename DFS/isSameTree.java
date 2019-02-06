458e4aa0e11ffa7b7086261493860c7f227f35be
Arjit <arjit32754@gmail.com>
2019-02-06 08:14:36 -0600

    recursive check for same tree

---
+++ DFS/isSameTree.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+   
+        if (p == q) {
+            return true;
+        }
+        
+      if (p == null || q == null) {
+            return false;
+        }
+        
+        
+        if (p.val == q.val) {
+            return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+        }
+      
+        return false;
+}
+}
