373aaf7431e46291e2ba368179391064709b1a87
Arjit <arjit32754@gmail.com>
2019-02-14 08:38:48 -0600

    maxDepth of binary tree

---
+++ DFS/maxDepth.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+    
+        return Math.max(l,r) + 1;
+    }
+}
