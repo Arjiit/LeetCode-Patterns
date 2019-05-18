f4ea46f042ffb6e41f92042fb619ad26f79d44f2
Arjit <arjit32754@gmail.com>
2019-05-17 21:09:58 -0700

    updated explanation

--- Dynamic Programming/coinChange2.java
+++ Dynamic Programming/coinChange2.java
@@ -6,6 +6,11 @@ class Solution {
         number of coins.
         bottom  up approach
         */
+        /*
+        Form a 2d array and each row consider addition of another new coin
+        and for each cell ask the question 'if you do use the additional coin or if you don't use'
+        so, we have dp[row][col] = dp[row-1][col] + dp[row][col - coins[row-1]]
+        */
         int[] dp = new int[amount + 1];
         dp[0] = 1; // no of ways to make sum equal to 0
             
