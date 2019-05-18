fc7054ddccbf84975a9d31803469e63678e15fde
Arjit <arjit32754@gmail.com>
2019-05-17 20:36:25 -0700

    explanation updated

--- Dynamic Programming/coinChange.java
+++ Dynamic Programming/coinChange.java
@@ -4,6 +4,12 @@ class Solution {
         Arrays.fill(dp, amount+1);
         dp[0] = 0; // so that when dp[j-coins[i]] becomes 0 we return 0. Since we have found the
         // total coins needed for the sum.
+        /*
+        for each coin we will look if we can form a minimum amount for amount ranging from 0 to
+        amount.
+        T[i] = min(T[i], 1 + T[i-coin])
+        minimum way to pick a amount = minimum(current way present to pick, if we pick the coin and then way to pick the remaining i.e. total - coin)
+        */
         for(int i=0; i<coins.length; i++) {
             for (int j = 1; j< amount+1; j++) {
                 if (coins[i] > j) {
