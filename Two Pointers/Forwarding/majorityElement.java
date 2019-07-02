af6d1ce47808addc43b4702e7af36d0c1385e071
Arjit <arjit32754@gmail.com>
2019-07-01 20:22:47 -0700

    bayer's moore voting algorithm basic

---
+++ Two Pointers/Forwarding/majorityElement.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+    /*
+        Bayer's moore voting algorithm.
+    */
+        int major = nums[0];
+        int count = 1;
+        for (int i =1; i<nums.length;i++) {
+            if (count == 0) {
+                count ++;
+                major = nums[i];
+            }
+            
+            else if (major == nums[i]) {
+                count ++;
+            }
+            
+            else {
+                count --;
+            }
+        }
+        return major;
+    }
+}
