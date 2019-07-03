841b470a9a5da43997c07b2cd8b93649502c2c2d
Arjit <arjit32754@gmail.com>
2019-07-02 18:43:08 -0700

    updated explanation

--- Dynamic Programming/uniquePaths.java
+++ Dynamic Programming/uniquePaths.java
@@ -1,14 +1,20 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m + 1][n + 1];
+        /*
+        we can see this as number of ways to reach a particular cell. For all the cells in the first row and 
+        for all the cells in the first columns, no of ways to reach is 1. For rest of the cells, the number
+        of ways to reach to them is number of way to reach from top plus number of ways to reach from the left.
+
+        */
+        int[][] dp = new int[m][n];
         
-        dp[0][0] = 1;
-        for (int i=1; i<m; i++){
+        
+        for (int i=0; i<m; i++){
             dp[i][0] = 1;
         }
         
         
-        for (int j=1; j<n; j++){
+        for (int j=0; j<n; j++){
             dp[0][j] = 1;
         }
         
