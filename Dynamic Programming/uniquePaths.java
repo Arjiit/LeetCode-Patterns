539e96aaf87d6890c00ffcbc3fab95d3de71b405
Arjit <arjit32754@gmail.com>
2019-07-02 18:45:09 -0700

    bug fixed

--- Dynamic Programming/uniquePaths.java
+++ Dynamic Programming/uniquePaths.java
@@ -18,8 +18,8 @@ class Solution {
             dp[0][j] = 1;
         }
         
-        for (int i=1; i < m; i++){
-            for (int j=1; j <= n; j++) {
+        for (int i=1; i < dp.length; i++){
+            for (int j=1; j < dp[i].length; j++) {
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
