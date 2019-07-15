bfda77bcb815446838a17937cee92a4e6849a02e
Arjit <arjit32754@gmail.com>
2019-07-14 22:50:37 -0700

    updated explanation

--- bit_manipulation/singleNumber3.java
+++ bit_manipulation/singleNumber3.java
@@ -1,5 +1,13 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
+        /*
+        We keep xoring elements in the array and since 0^N = N and N^N = 0, then result would have a^b since a
+        and b occur just once. So, we could also be sure that there would definitely be a 1 in the result as 
+        a and b are different numbers. So, we have to find that last set bit, which we can find by this formula
+        diff = diff & ~(diff - 1), this would make the last set bit as 1 and rest 0. So, this is the bit which 
+        differs in a and b. So, we loop throught the array again and check the elements having this set bit 
+        and this bit unset and xor in those two halves to find a and b.
+        */
         // Pass 1 : 
         // Get the XOR of the two numbers we need to find
         int diff = 0;
