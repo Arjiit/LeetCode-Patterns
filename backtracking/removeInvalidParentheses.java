a5e027801b7264d0bfc09599a06a6155048c408d
Arjit <arjit32754@gmail.com>
2019-07-20 20:07:26 -0700

    updated explanation, reusing same code to find invalid parenthesis

--- backtracking/removeInvalidParentheses.java
+++ backtracking/removeInvalidParentheses.java
@@ -5,6 +5,17 @@ class Solution {
         return output;
     }
 
+    /*
+    We keep the openParen as '(' and closed as ')', we try to find an extra closed ')' that would make an invalid parenthesis
+    and we will remove it by skipping it and then keep checking for invalid parenthesis with iStart value as next i value and
+    jStart value as the index of j we removed the invalid parenthesis from. If we are done 'i' iterating over the string, we 
+    reverse the string and now we check for invalid parenthesis with an extra '(' and so, we call the same code recursively.
+    once we are done, so we reverse back to original valid string and add to result. The control goes back to return statement
+    to find the next ')' since j goes from jStart to i.
+    The program only looks for valid answers which in this case if it is 'k', then time complexity would be O(nk) where n is
+    the length of the string.
+    */
+
     public void removeHelper(String s, List<String> output, int iStart, int jStart, char openParen, char closedParen) {
         int numOpenParen = 0, numClosedParen = 0;
         for (int i = iStart; i < s.length(); i++) {
@@ -20,9 +31,9 @@ class Solution {
         }
         // No invalid closed parenthesis detected. Now check opposite direction, or reverse back to original direction.
         String reversed = new StringBuilder(s).reverse().toString();
-        if (openParen == '(')
+        if (openParen == '(') // to check wheteher we have just checked for the original version of the string or reverse.
             removeHelper(reversed, output, 0, 0, ')','(');
         else
-            output.add(reversed);
+            output.add(reversed); // we have checked both the original and reversed version.
     }
 }
