5f31174e3476aef392009708e7400894aa36e565
Arjit <arjit32754@gmail.com>
2019-11-10 00:01:45 -0600

    Update queryString.java

--- Strings/queryString.java
+++ Strings/queryString.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean queryString(String S, int N) {
+        /*
+        The solution can be improve a half by checking from N to N/2.
+The reason is simply for every i < N/2, the binary string of 2*i will contain binary string of i. Thus we don't need to check for i < N/2
+        */
         int n = 1;
         List<String> res = new ArrayList<>();
         while (n <= N) {
