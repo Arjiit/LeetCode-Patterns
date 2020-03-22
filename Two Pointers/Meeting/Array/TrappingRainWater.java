89157aee7a12b5a71ed91d0409f96dca32cd82ea
Arjit <arjit32754@gmail.com>
2020-03-22 10:41:23 -0500

    updating with two more solutions

--- Two Pointers/Meeting/Array/TrappingRainWater.java
+++ Two Pointers/Meeting/Array/TrappingRainWater.java
@@ -1,3 +1,69 @@
+
+// O(N^2) solution
+class Solution {
+    public int trap(int[] height) {
+        int total = 0;
+        for (int i=1; i<height.length; i++) {
+            int cur_left= max(height, 0, i);
+            //System.out.println(cur_left);
+            int cur_right = max(height, i, height.length-1);
+            //System.out.println(cur_right);
+            int cur_max = Math.min(cur_left, cur_right);
+            total = total + cur_max - height[i];
+        }
+        return total;
+    }
+    
+    public int max(int[] height, int left, int right) {
+        int max = 0;
+        for (int i=left; i<=right; i++) {
+            if (height[i] > max) {
+                max = height[i];
+            }
+        }
+        return max;
+    }
+}
+
+// O(N) time, O(N) space
+class Solution {
+    public int trap(int[] height) {
+        int[] left = precomputeLeft(height);
+        int[] right = preComputeRight(height);
+        int total = 0;
+        for (int i=0; i<height.length; i++) {
+            
+            total += Math.min(left[i], right[i]) - height[i];
+        }
+        return total;
+    }
+    
+    public int[] precomputeLeft(int[] height) {
+        int[] res = new int[height.length];
+        if (height.length == 0) {
+            return res;
+        }
+        res[0] = height[0];
+        for (int i=1; i<height.length; i++) {
+            res[i] = Math.max(res[i-1], height[i]);
+        }
+        return res;
+    }
+    
+    public int[] preComputeRight(int[] height) {
+        int[] res = new int[height.length];
+        if (height.length == 0) {
+            return res;
+        }
+        res[height.length - 1] = height[height.length -1];
+        for (int i=height.length - 2; i>= 0; i--) {
+            res[i] = Math.max(res[i+1], height[i]);
+        }
+        return res;
+    }
+}
+
+// O(N) time. O(1) space using lower envelope technique
 class Solution {
     public int trap(int[] height) {
         int leftMax = 0;
